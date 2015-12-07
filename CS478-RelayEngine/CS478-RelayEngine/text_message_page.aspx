9484f7f617c0ecdfa993a76c851580b0dba5618d
sritz <shaneritz@gmail.com>
2015-12-07 17:56:19 -0600

    add page

---
+++ CS478-RelayEngine/CS478-RelayEngine/text_message_page.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="text_message_page.aspx.cs" Inherits="CS478_RelayEngine.text_message_page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 385px">
+    <form id="form1" runat="server">
+    <div style="width: 960px">
+    
+        <asp:Label ID="Label1" runat="server" Text="User:"></asp:Label>
+        &nbsp;&nbsp;<asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label3" runat="server" Text="Tokens Remaining:          "></asp:Label>
+        <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+    
+    </div>
+        <div style="height: 131px">
+            <asp:Label ID="Label4" runat="server" Text="Lists" style="font-weight: 700; text-decoration: underline"></asp:Label>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="School Cancellation/Delays" Visible="False" />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:CheckBox ID="CheckBox2" runat="server" Text="Upper School Sports" Visible="False" />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:CheckBox ID="CheckBox3" runat="server" Text="Lower School Sports" Visible="False" />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:CheckBox ID="CheckBox4" runat="server" Text="Upper School Announcements" Visible="False" />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:CheckBox ID="CheckBox5" runat="server" Text="Lower School Announcements" Visible="False" />
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="text-align: left" Text="Submit" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Cancel" />
+    </form>
+</body>
+</html>
---
+++ CS478-RelayEngine/CS478-RelayEngine/text_message_page.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS478_RelayEngine
+{
+    public partial class text_message_page : System.Web.UI.Page
+    {
+        public static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Shane\\Documents\\CS478-RelayEngine\\CS478-RelayEngine\\CS478-RelayEngine\\App_Data\\Project_Database.mdf;Integrated Security=True";
+        public static string testSender = "egestas@mus.com";
+        int list_id;
+        int tokens;
+        int sub_one = 0, sub_two = 0, sub_three = 0, sub_four = 0, sub_five = 0;
+        int recipients = 0;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                if (CheckBox1.Checked == true) recipients = sub_one;
+                else if (CheckBox2.Checked == true) recipients = sub_two;
+                else if (CheckBox3.Checked == true) recipients = sub_three;
+                else if (CheckBox4.Checked == true) recipients = sub_four;
+                else if (CheckBox5.Checked == true) recipients = sub_five;
+
+                con.Open();
+                String commandText = "UPDATE ADMINISTRATOR SET ADMIN_TOKENS=ADMIN_TOKENS-" + recipients.ToString() + ";";
+
+                SqlCommand cmd = new SqlCommand(commandText, con);
+                cmd.ExecuteReader();
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label5.Text = testSender;
+
+            SqlDataReader dr = null;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                String commandText = "SELECT LIST_ID FROM LIST WHERE LIST_ID IN (SELECT LIST_ID FROM AUTHORIZATIONS WHERE SENDER_ID IN (SELECT SENDER_ID FROM SENDER WHERE SENDER_EMAIL='egestas@mus.com'));";
+
+                using (SqlCommand cmd = new SqlCommand(commandText,con))
+                {
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        list_id = dr.GetInt32(0);
+                        checkLists();
+                    }
+                    dr.Close();
+                }
+                
+            }
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                String commandText = "SELECT LIST_ID FROM SUBSCRIPTION;";
+
+                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                {
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        int id = dr.GetInt32(0);
+                        if (id == 0) sub_one++;
+                        else if (id == 1) sub_two++;
+                        else if (id == 2) sub_two++;
+                        else if (id == 3) sub_three++;
+                        else if (id == 4) sub_four++;
+                    }
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                String commandText = "SELECT ADMIN_TOKENS FROM ADMINISTRATOR;";
+
+                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                {
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        tokens = dr.GetInt32(0);
+                        checkTokens();
+                    }
+                    dr.Close();
+                }
+            }
+        }
+
+        public void checkLists()
+        {
+            if (list_id == 0) CheckBox1.Visible = true;
+            else if (list_id == 1) CheckBox2.Visible = true;
+            else if (list_id == 2) CheckBox3.Visible = true;
+            else if (list_id == 3) CheckBox4.Visible = true;
+            else if (list_id == 4) CheckBox5.Visible = true;
+        }
+
+        public void checkTokens()
+        {
+            if (sub_one > tokens)
+            {
+                CheckBox1.Enabled = false;
+                CheckBox1.Text = "Insufficient number of tokens to send this message";
+                CheckBox1.ForeColor = System.Drawing.Color.Red;
+            }
+            if (sub_two > tokens)
+            {
+                CheckBox2.Enabled = false;
+                CheckBox2.Text = "Insufficient number of tokens to send this message";
+                CheckBox2.ForeColor = System.Drawing.Color.Red;
+            }
+            if (sub_three > tokens)
+            {
+                CheckBox3.Enabled = false;
+                CheckBox3.Text = "Insufficient number of tokens to send this message";
+                CheckBox3.ForeColor = System.Drawing.Color.Red;
+            }
+            if (sub_four > tokens)
+            {
+                CheckBox4.Enabled = false;
+                CheckBox4.Text = "Insufficient number of tokens to send this message";
+                CheckBox4.ForeColor = System.Drawing.Color.Red;
+            }
+            if (sub_five > tokens)
+            {
+                CheckBox5.Enabled = false;
+                CheckBox5.Text = "Insufficient number of tokens to send this message";
+                CheckBox5.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
