a4f419a40adff68ba3c43501e048a9b4f7d3f191
Usiwallabies <quokka1>
2015-12-08 00:22:49 -0600

    Added Organization Security Code Page
    The code is sent to the database in ADMINISTRATOR to the field
    ADMIN_SECURITYCODE.

---
+++ CS478-RelayEngine/CS478-RelayEngine/Organization Security Code Page.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Organization Security Code Page.aspx.cs" Inherits="CS478_RelayEngine.Organization_Security_Code_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <div>
+    
+        <asp:Panel ID="Panel1" runat="server" style="text-align: center">
+            <asp:Image ID="LogoImage" runat="server" ImageUrl="~/Content/evansville_day_school.jpg" />
+            &nbsp;<br />
+        </asp:Panel>
+    
+    </div>
+        <asp:Panel ID="Panel2" runat="server" CssClass="textAlertListPanelClass" style="text-align: center">
+            <br />
+            Here you can decide whether or not people will need to enter a security code in order to sign up for alerts.<br />
+            <asp:Label ID="Label2" runat="server" Font-Bold="True" Text="Current Code: "></asp:Label>
+            <br />
+            <br />
+        </asp:Panel>
+        <asp:Panel ID="Panel3" runat="server" Wrap="False" style ="text-align:center">
+            <br />
+            <asp:RadioButtonList ID="RadioButtonList" runat="server" Font-Size="Larger" align="center">
+                <asp:ListItem Value="Activated">Activate security code. Users will need a code to register for alerts.</asp:ListItem>
+                <asp:ListItem Value="Deactivated">Deactivate security code. Users will be able to freely register for alerts.</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+        </asp:Panel>
+        <asp:Panel ID="Panel5" runat="server" style="text-align: center; height: 64px">
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Organization Security Code:"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server" Width="185px" AutoPostBack="True"></asp:TextBox>
+        </asp:Panel>
+        <asp:Panel ID="Panel4" runat="server" style="text-align: center; " Height="64px">
+            <br />
+            <asp:Button ID="SaveChangesButton" runat="server" Text="Save Changes" Width="100px" OnClick="SaveChangesButton_Click"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="CancelButton" runat="server" Text="Cancel" Width="100px" OnClick="CancelButton_Click" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ CS478-RelayEngine/CS478-RelayEngine/Organization Security Code Page.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS478_RelayEngine
+{
+    public partial class Organization_Security_Code_Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Load_Logo();
+
+            string connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+            string selstring = "SELECT ADMIN_SECURITYCODE FROM ADMINISTRATOR WHERE USER_ID = 0";
+            SqlDataSource db = new SqlDataSource(connstring, selstring);
+            DataView dv = (DataView)db.Select(DataSourceSelectArguments.Empty);
+
+            string code;
+            try
+            {
+                code = (string)dv.Table.Rows[0][0];
+            }
+            catch (Exception)
+            {
+                code = "No Code Set";
+            }
+
+
+            Label2.Text = "Current Code: " + code;
+            
+            
+
+            dv.Dispose();
+        }
+
+        private void Load_Logo()
+        {
+            string connectionstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+            string selectstring = "SELECT ADMIN_LOGONAME FROM ADMINISTRATOR WHERE USER_ID = 0";
+
+            SqlDataSource database = new SqlDataSource(connectionstring, selectstring);
+            DataView dv = (DataView)database.Select(DataSourceSelectArguments.Empty);
+            string filename = (string)dv.Table.Rows[0][0];
+
+            if (System.IO.File.Exists(Server.MapPath("~/Content/") + filename) == true)
+            {
+                LogoImage.ImageUrl = "~/Content/" + filename;
+            }
+            else
+            {
+                LogoImage.ImageUrl = "~/Content/" + "evansville_day_school.jpg";
+
+                string connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+                string selstring = "SELECT * FROM ADMINISTRATOR WHERE USER_ID = 0";
+                string updatestring = "UPDATE ADMINISTRATOR SET ADMIN_LOGONAME = 'evansville_day_school.jpg' WHERE USER_ID = 0";
+                SqlDataSource db = new SqlDataSource(connstring, selstring);
+                db.UpdateCommand = updatestring;
+                db.Update();
+            }
+
+            dv.Dispose();
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Organization Management Page.aspx");
+        }
+
+        protected void SaveChangesButton_Click(object sender, EventArgs e)
+        {
+            string securitycode;
+            string connstring;
+            string selstring;
+            string updatestring;
+            SqlDataSource db;
+
+            if (RadioButtonList.SelectedValue == "Activated")
+            {
+                
+                securitycode = TextBox1.Text;
+
+                connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+                selstring = "SELECT * FROM ADMINISTRATOR WHERE USER_ID = 0";
+                updatestring = string.Format("UPDATE ADMINISTRATOR SET ADMIN_SECURITYCODE = '{0}' WHERE USER_ID = 0", securitycode);
+                db = new SqlDataSource(connstring, selstring);
+                db.UpdateCommand = updatestring;
+                db.Update();
+            }
+            else if (RadioButtonList.SelectedValue == "Deactivated")
+            {
+                securitycode = "No Code Set";
+
+                connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+                selstring = "SELECT * FROM ADMINISTRATOR WHERE USER_ID = 0";
+                updatestring = string.Format("UPDATE ADMINISTRATOR SET ADMIN_SECURITYCODE = '{0}' WHERE USER_ID = 0", securitycode);
+                db = new SqlDataSource(connstring, selstring);
+                db.UpdateCommand = updatestring;
+                db.Update();
+            }
+            else
+            {
+            }
+
+            Server.Transfer("Organization Management Page.aspx");
+        }
+    }
+}
