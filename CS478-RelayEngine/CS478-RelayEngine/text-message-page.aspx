3627d593ffe99f0c9e11d0a3b98e46d87c1078cb
sritz <shaneritz@gmail.com>
2015-12-09 14:55:36 -0600

    Update Layout
    Also fixed an issue where the submit button sometimes need to be clicked
    twice, and used some Javascript to make the page count the number of
    characters in the message box much smoother.

--- CS478-RelayEngine/CS478-RelayEngine/text-message-page.aspx
+++ CS478-RelayEngine/CS478-RelayEngine/text-message-page.aspx
@@ -4,48 +4,60 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <script type ="text/javascript">
+        function count(txt)
+        {
+            var lbl = document.getElementById("Label7");
+            lbl.innerHTML = "Message (160 characters max) " + txt.value.length + "/160";
+        }
+    </script>
     <title></title>
 </head>
 <body style="height: 385px">
     <form id="form1" runat="server">
         
-    <div style="width: 960px">
     
-            <asp:Image ID="LogoImage" runat="server" ImageUrl="~/Content/evansville_day_school.jpg" />
+    <div style="text-align:center">    
+            <asp:Image ID="LogoImage" runat="server" ImageUrl="~/Content/evansville_day_school.jpg" ImageAlign="AbsMiddle" />
             <br />
     
         <asp:Label ID="Label1" runat="server" Text="User:"></asp:Label>
         &nbsp;&nbsp;<asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <asp:Label ID="Label3" runat="server" Text="Tokens Remaining:          "></asp:Label>
         <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
         <br />
-        <br />
-    
+        <br />    
     </div>
-        <div style="height: 320px">
+
+        <div style="text-align:center">
             <asp:Label ID="Label4" runat="server" Text="Lists" style="font-weight: 700; text-decoration: underline"></asp:Label>
             <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="School Cancellation/Delays" Visible="False" />
             <br />
             <asp:CheckBox ID="CheckBox2" runat="server" OnCheckChange="CheckBox2_CheckChanged" Text="Upper School Sports" Visible="False" />
             <br />
             <asp:CheckBox ID="CheckBox3" runat="server" OnCheckChange="CheckBox3_CheckChanged" Text="Lower School Sports" Visible="False" />
             <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <asp:CheckBox ID="CheckBox4" runat="server" OnCheckChange="CheckBox4_CheckChanged" Text="Upper School Announcements" Visible="False" />
             <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <asp:CheckBox ID="CheckBox5" runat="server" OnCheckChange="CheckBox5_CheckChanged" Text="Lower School Announcements" Visible="False" />
             <br />
             <br />
             <asp:Label ID="Label7" runat="server" Text="Message (160 Characters Max)"></asp:Label>
             <br />
-            <asp:TextBox ID="TextBox1" runat="server" Height="117px" Rows="5" TextMode="MultiLine" Width="383px" OnTextChanged="TextBox1_TextChanged" AutoPostBack="true"></asp:TextBox>
+            <asp:TextBox ID="TextBox1" runat="server" Height="117px" Rows="5" TextMode="MultiLine" Width="383px" onkeyup="count(this)" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
         </div>
+        <div style="text-align:center">
         <asp:Label ID="Label8" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
         <br />
         <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="text-align: left" Text="Submit" />
 &nbsp;&nbsp;
         <asp:Button ID="Button2" runat="server" Text="Cancel" />
+        </div>
     </form>
 </body>
 </html>
--- CS478-RelayEngine/CS478-RelayEngine/text-message-page.aspx.cs
+++ CS478-RelayEngine/CS478-RelayEngine/text-message-page.aspx.cs
@@ -19,74 +19,76 @@ namespace CS478_RelayEngine
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Load_Logo();
-
-            Label5.Text = testSender;
-
-            SqlDataReader dr = null;
-
-            using (SqlConnection con = new SqlConnection(connectionString))
+            if (!this.IsPostBack)
             {
-                con.Open();
-                String commandText = "SELECT LIST_ID FROM LIST WHERE LIST_ID IN (SELECT LIST_ID FROM AUTHORIZATIONS WHERE SENDER_ID IN (SELECT SENDER_ID FROM SENDER WHERE SENDER_EMAIL='egestas@mus.com'));";
+                Load_Logo();
+                Label5.Text = testSender;
 
-                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                SqlDataReader dr = null;
+
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    dr = cmd.ExecuteReader();
-                    while (dr.Read())
+                    con.Open();
+                    String commandText = "SELECT LIST_ID FROM LIST WHERE LIST_ID IN (SELECT LIST_ID FROM AUTHORIZATIONS WHERE SENDER_ID IN (SELECT SENDER_ID FROM SENDER WHERE SENDER_EMAIL='egestas@mus.com'));";
+
+                    using (SqlCommand cmd = new SqlCommand(commandText, con))
                     {
-                        list_id = dr.GetInt32(0);
-                        checkLists();
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            list_id = dr.GetInt32(0);
+                            checkLists();
+                        }
+                        dr.Close();
                     }
-                    dr.Close();
+
                 }
 
-            }
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();
-                String commandText = "SELECT LIST_ID FROM SUBSCRIPTION;";
-
-                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    dr = cmd.ExecuteReader();
-                    while (dr.Read())
-                    {
-                        int id = dr.GetInt32(0);
+                    con.Open();
+                    String commandText = "SELECT LIST_ID FROM SUBSCRIPTION;";
 
-                        if (id == 0) sub_one++;
-                        else if (id == 1) sub_two++;
-                        else if (id == 2) sub_two++;
-                        else if (id == 3) sub_three++;
-                        else if (id == 4) sub_four++;
+                    using (SqlCommand cmd = new SqlCommand(commandText, con))
+                    {
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            int id = dr.GetInt32(0);
+
+                            if (id == 0) sub_one++;
+                            else if (id == 1) sub_two++;
+                            else if (id == 2) sub_two++;
+                            else if (id == 3) sub_three++;
+                            else if (id == 4) sub_four++;
+                        }
                     }
                 }
-            }
 
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();
-                String commandText = "SELECT ADMIN_TOKENS FROM ADMINISTRATOR;";
-
-                using (SqlCommand cmd = new SqlCommand(commandText, con))
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    dr = cmd.ExecuteReader();
-                    while (dr.Read())
-                    {
-                        tokens = dr.GetInt32(0);
-                        Label6.Text = tokens.ToString();
-                        checkTokens();
-                    }
-                    dr.Close();
-                }
-            }
+                    con.Open();
+                    String commandText = "SELECT ADMIN_TOKENS FROM ADMINISTRATOR;";
 
-            CheckBox1.Text = "School Cancellation/Delays (" + sub_one.ToString() + " recipients)";
-            CheckBox2.Text = "Upper School Sports (" + sub_two.ToString() + " recipients)";
-            CheckBox3.Text = "Lower School Sports (" + sub_three.ToString() + " recipients)";
-            CheckBox4.Text = "Upper School Announcements (" + sub_four.ToString() + " recipients)";
-            CheckBox5.Text = "Lower School Announcements (" + sub_five.ToString() + " recipients)";
+                    using (SqlCommand cmd = new SqlCommand(commandText, con))
+                    {
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            tokens = dr.GetInt32(0);
+                            Label6.Text = tokens.ToString();
+                            checkTokens();
+                        }
+                        dr.Close();
+                    }
+                }
+
+                CheckBox1.Text = "School Cancellation/Delays (" + sub_one.ToString() + " recipients)";
+                CheckBox2.Text = "Upper School Sports (" + sub_two.ToString() + " recipients)";
+                CheckBox3.Text = "Lower School Sports (" + sub_three.ToString() + " recipients)";
+                CheckBox4.Text = "Upper School Announcements (" + sub_four.ToString() + " recipients)";
+                CheckBox5.Text = "Lower School Announcements (" + sub_five.ToString() + " recipients)";
+            }
         }
 
         private void Load_Logo()
@@ -197,7 +199,7 @@ namespace CS478_RelayEngine
         }
         protected void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            Label7.Text = "Message (160 Characters Max)   " + TextBox1.Text.Length + "/160";
+            
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -207,7 +209,7 @@ namespace CS478_RelayEngine
                 Label8.Text = "Unable to send message: your message contains too many characters.";
                 Label8.Visible = true;
             }
-            else if (TextBox1.Text.Length == 0)
+            else if (TextBox1.Text == "")
             {
                 Label8.Text = "Unable to send message: message is blank.";
                 Label8.Visible = true;
