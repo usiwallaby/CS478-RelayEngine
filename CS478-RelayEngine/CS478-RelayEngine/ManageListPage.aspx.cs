c3ad4c8ea1d8b1e49ad02a260d2a25f8abcb123e
PTSFloyd <ptsfloyd42@hotmail.com>
2015-12-09 22:23:32 -0600

    Update Manage List Page

--- CS478-RelayEngine/CS478-RelayEngine/ManageListPage.aspx.cs
+++ CS478-RelayEngine/CS478-RelayEngine/ManageListPage.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
@@ -10,11 +13,15 @@ namespace CS478_RelayEngine
         protected void Page_Load(object sender, EventArgs e)
         {
             Load_Logo();
+
+            Label2.Text = "CurrentList";
+            //Label2.Text = Session["CurrentList"].ToString();
+           
         }
 
         private void Load_Logo()
         {
-            string connectionstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\EvansvilleDaySchoolDatabase.mdf;Integrated Security=True";
+            string connectionstring = "Server=tcp:evansvilledayschoolserver.database.windows.net,1433;Database=EvansvilleDaySchoolDatabase;User ID=Usiwallabies@evansvilledayschoolserver;Password=Quokka12;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             string selectstring = "SELECT ADMIN_LOGONAME FROM ADMINISTRATOR WHERE USER_ID = 0";
 
             SqlDataSource database = new SqlDataSource(connectionstring, selectstring);
@@ -29,7 +36,7 @@ namespace CS478_RelayEngine
             {
                 LogoImage.ImageUrl = "~/Content/" + "evansville_day_school.jpg";
 
-                string connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Project_Database.mdf;Integrated Security=True";
+                string connstring = "Server=tcp:evansvilledayschoolserver.database.windows.net,1433;Database=EvansvilleDaySchoolDatabase;User ID=Usiwallabies@evansvilledayschoolserver;Password=Quokka12;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 string selstring = "SELECT * FROM ADMINISTRATOR WHERE USER_ID = 0";
                 string updatestring = "UPDATE ADMINISTRATOR SET ADMIN_LOGONAME = 'evansville_day_school.jpg' WHERE USER_ID = 0";
                 SqlDataSource db = new SqlDataSource(connstring, selstring);
@@ -38,6 +45,29 @@ namespace CS478_RelayEngine
             }
 
             dv.Dispose();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox3.Text == "987")
+            {
+                Response.Write("<script>alert('Clear list subs.')</script>");
+            }
+
+            if (SendConfirmation.Checked)
+            {
+                Response.Write("<script>alert('Confirmation has been sent!')</script>");
+            }
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendConfirmation_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
